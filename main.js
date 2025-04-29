3b27118da8a3ca263c99d0f64bac30dfee1ec446
Horizon <mgthedasher@gmail.com>
2025-04-29 21:18:04 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 320,
+    height: 240,
+    frame: false,
+    alwaysOnTop: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile(path.join(__dirname, 'renderer', 'src', 'index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
