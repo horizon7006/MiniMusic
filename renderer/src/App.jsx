07568496cc2a6a3e72612881a1ef454acee6e299
Horizon <mgthedasher@gmail.com>
2025-04-29 21:19:39 +0300

    Create App.jsx

---
+++ renderer/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">MiniMusic</h1>
+      {/* UI goes here */}
+    </div>
+  );
+}
